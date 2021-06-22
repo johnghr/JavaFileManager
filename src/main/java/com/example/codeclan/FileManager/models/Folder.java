9dd641bc0f336db8b61d7a35395ed8ecad0cd338
John Ross <johnyboy11@hotmail.co.uk>
2021-06-22 17:06:25 +0100

    "Unrelated folder class setup"

---
+++ src/main/java/com/example/codeclan/FileManager/models/Folder.java
@@ -0,0 +1,55 @@
+package com.example.codeclan.FileManager.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "folder")
+    private List<File> files;
+
+    public Folder(String name) {
+        this.name = name;
+        this.files = new ArrayList<>();
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
